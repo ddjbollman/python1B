fe7dcaa2735bae7b0631e4afc0f57dbca8c9bf7b
joseph bollman <joseph.bollman@gmail.com>
2017-10-27 17:06:58 -0700

    adding book to tweet.  commented out print line to invoking api and updating status

--- project.py
+++ project.py
@@ -1,5 +1,5 @@
 import tweepy
-
+from time import sleep
 from secrets import *
 
 
@@ -13,3 +13,30 @@ api = tweepy.API(auth)
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
     print(tweet.text)
+
+# open file under default read mode
+my_file = open('20ksea.txt', 'r')
+
+# read each line and save it to a variable
+file_lines = my_file.readlines()
+
+# close file, good behavior.
+my_file.close()
+
+
+for line in file_lines:
+    try:
+        if line != '\n':
+            api.update_status(line)
+            #print(line)
+            sleep(900)
+        else:
+            pass
+    except tweepy.TweepError as e:
+        print(e.reason)
+        sleep(2)
+
+
+
+
+file_lines.
